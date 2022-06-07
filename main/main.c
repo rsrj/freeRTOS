6b4686d929edd73b1b69ce55b73890440069d24b
Railton Rocha <railton.rocha@PEINSMTNB2673.insole.local>
2022-06-07 20:34:06 -0300

    Changed function xTaskNofifyGive to xTaskNotify to send different data in the task notification

--- main/main.c
+++ main/main.c
@@ -8,23 +8,24 @@ void sender(void * params)
 {
     while (true)
     {
-        xTaskNotifyGive(xReceiverHandler); /*Send notification to the receiver() task, bringing it out of the blocked state*/
-        xTaskNotifyGive(xReceiverHandler);
-        xTaskNotifyGive(xReceiverHandler);
-        xTaskNotifyGive(xReceiverHandler);
-        vTaskDelay(5000 / portTICK_RATE_MS);
+        xTaskNotify( xReceiverHandler, (1 << 0), eSetValueWithoutOverwrite );
+        vTaskDelay(1000 / portTICK_RATE_MS);
+        xTaskNotify( xReceiverHandler, (1 << 1), eSetValueWithoutOverwrite );
+        vTaskDelay(1000 / portTICK_RATE_MS);
+        xTaskNotify( xReceiverHandler, (1 << 2), eSetValueWithoutOverwrite );
+        vTaskDelay(1000 / portTICK_RATE_MS);
+        xTaskNotify( xReceiverHandler, (1 << 3), eSetValueWithoutOverwrite );
+        vTaskDelay(1000 / portTICK_RATE_MS);
     }
 }
 
 void receiver(void * params)
 {
+    uint32_t state;
     while (true)
     {
-
-        /*If xClearCountOnExit is pdTRUE it executes only once and clears the counter, 
-        otherwise, if pdFALSE it executes and decrements the counter until it reaches zero.*/
-        uint32_t count = ulTaskNotifyTake( pdFALSE, portMAX_DELAY ); 
-        printf("Received notification %d times\n", count);
+        xTaskNotifyWait( 0, 0, &state, portMAX_DELAY); 
+        printf("Received state %d \n", state);
     }
 }
 

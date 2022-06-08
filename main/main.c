6f85acac5edb3d224b38302aedb6da8a60b308fb
Railton Rocha <railton.rocha@PEINSMTNB2673.insole.local>
2022-06-08 15:24:19 -0300

    Implemented a Mutex semaphore

--- main/main.c
+++ main/main.c
@@ -1,36 +1,69 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/semphr.h"
 
-static TaskHandle_t xReceiverHandler = NULL, xSenderHandler = NULL;
+/** MUTEX EXAMPLE
+ * 
+ *  Mutual exclusion
+ *  It's a type of semaphore that only the task that blocks a another task can unblock it
+ *  It is helpful when one task cannot call another task until all the work had finished inside it.
+ *  
+ */
+/*Creates a handle that will be used by tasks*/
+xSemaphoreHandle mutexBus;
 
-void sender(void * params)
+/*Function that will be handled and blocked until the end processing*/
+void writeToBus(char * message)
+{
+    printf(message);
+}
+
+void task1(void * params)
 {
     while (true)
     {
-        xTaskNotify( xReceiverHandler, (1 << 0), eSetValueWithoutOverwrite );
-        vTaskDelay(1000 / portTICK_RATE_MS);
-        xTaskNotify( xReceiverHandler, (1 << 1), eSetValueWithoutOverwrite );
-        vTaskDelay(1000 / portTICK_RATE_MS);
-        xTaskNotify( xReceiverHandler, (1 << 2), eSetValueWithoutOverwrite );
-        vTaskDelay(1000 / portTICK_RATE_MS);
-        xTaskNotify( xReceiverHandler, (1 << 3), eSetValueWithoutOverwrite );
-        vTaskDelay(1000 / portTICK_RATE_MS);
+        printf("Reading temperature\n");
+        /*Blocks the mutexBus*/
+        if (xSemaphoreTake(mutexBus, 1000 / portTICK_PERIOD_MS))
+        {
+            writeToBus("Temperature is 25C\n");
+            /*Unblocks the mutex to be used by another task*/
+            xSemaphoreGive(mutexBus);
+        }
+        else 
+        {
+            printf("Writing temperature timed out\n");
+        }
+        
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
 
-void receiver(void * params)
+void task2(void * params)
 {
-    uint32_t state;
     while (true)
     {
-        xTaskNotifyWait( 0, 0, &state, portMAX_DELAY); 
-        printf("Received state %d \n", state);
+        printf("Reading humidity\n");
+        if (xSemaphoreTake(mutexBus, 1000 / portTICK_PERIOD_MS))
+        {
+            writeToBus("Humidity is 50\n");
+            xSemaphoreGive(mutexBus);
+        }
+        else
+        {
+            printf("Writing humidity timed out\n");
+        }
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
     }
 }
 
 void app_main(void)
 {
-    xTaskCreate(&receiver, "receiver", 2048, NULL, 2, &xReceiverHandler);
-    xTaskCreate(&sender, "sender", 2048, NULL, 2, &xSenderHandler);
+    /*Creating the Mutex semaphore and associate it with the semaphore handler*/
+    mutexBus = xSemaphoreCreateMutex();
+    xTaskCreate(&task1, "temperature", 2048, NULL, 2, NULL);
+    xTaskCreate(&task2, "humidity", 2048, NULL, 2, NULL);
 }

9b3e682b9c973408796afaa81e79fd0a90543b64
Railton Rocha <railton.rocha@PEINSMTNB2673.insole.local>
2022-06-08 17:59:31 -0300

    Implemented queue example

--- main/main.c
+++ main/main.c
@@ -1,35 +1,48 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
 
-static TaskHandle_t xReceiverHandler = NULL, xSenderHandler = NULL;
+xQueueHandle queue;
 
-void sender(void * params)
+void listenForHttp(void * params)
 {
+    int count = 0;
     while (true)
     {
-        xTaskNotifyGive(xReceiverHandler); /*Send notification to the receiver() task, bringing it out of the blocked state*/
-        xTaskNotifyGive(xReceiverHandler);
-        xTaskNotifyGive(xReceiverHandler);
-        xTaskNotifyGive(xReceiverHandler);
-        vTaskDelay(5000 / portTICK_RATE_MS);
+        count++;
+        printf("received http message\n");
+        long ok = xQueueSend(queue, &count, 1000 / portTICK_PERIOD_MS);
+        if (ok)
+        {
+            printf("Added message to queue\n");
+        }
+        else
+        {
+            printf("failed to add message to queue\n");
+        }
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
 
-void receiver(void * params)
+void task1(void * params)
 {
     while (true)
     {
-
-        /*If xClearCountOnExit is pdTRUE it executes only once and clears the counter, 
-        otherwise, if pdFALSE it executes and decrements the counter until it reaches zero.*/
-        uint32_t count = ulTaskNotifyTake( pdFALSE, portMAX_DELAY ); 
-        printf("Received notification %d times\n", count);
+        int rxInt;
+        if(xQueueReceive(queue, &rxInt, 5000 / portTICK_PERIOD_MS))
+        {
+            printf("Integer received: %d\n", rxInt);
+        }
     }
 }
 
 void app_main(void)
 {
-    xTaskCreate(&receiver, "receiver", 2048, NULL, 2, &xReceiverHandler);
-    xTaskCreate(&sender, "sender", 2048, NULL, 2, &xSenderHandler);
+    /*To create a queue is necessary to specify the number of elements and the queue size,
+    once created, if the queue is full, may be not possible to put others elements until
+    the data is treated*/
+    queue = xQueueCreate(3, sizeof(int));
+    xTaskCreate(&listenForHttp, "get http", 2048, NULL, 2, NULL);
+    xTaskCreate(&task1, "do something with http", 2048, NULL, 2, NULL);
 }

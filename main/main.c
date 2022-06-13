da1ecdbeaba352940ff8dd9cb61bb6c24d1a71d9
Railton Rocha <railton.rocha@PEINSMTNB2673.insole.local>
2022-06-13 17:18:10 -0300

    Implemented calculator using function pointer

--- main/main.c
+++ main/main.c
@@ -1,35 +1,114 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include <string.h>
+#include "esp_system.h"
+#include "esp_console.h"
+#include "esp_vfs_dev.h"
+#include "esp_vfs_fat.h"
+#include "driver/uart.h"
 
-static TaskHandle_t xReceiverHandler = NULL, xSenderHandler = NULL;
+typedef float (*calculator)(float, float); //Declaring a function pointer
 
-void sender(void * params)
+float sum ( float a , float b )
 {
-    while (true)
-    {
-        xTaskNotifyGive(xReceiverHandler); /*Send notification to the receiver() task, bringing it out of the blocked state*/
-        xTaskNotifyGive(xReceiverHandler);
-        xTaskNotifyGive(xReceiverHandler);
-        xTaskNotifyGive(xReceiverHandler);
-        vTaskDelay(5000 / portTICK_RATE_MS);
-    }
+    return a + b;
 }
 
-void receiver(void * params)
+float mult ( float a , float b)
 {
-    while (true)
-    {
-
-        /*If xClearCountOnExit is pdTRUE it executes only once and clears the counter, 
-        otherwise, if pdFALSE it executes and decrements the counter until it reaches zero.*/
-        uint32_t count = ulTaskNotifyTake( pdFALSE, portMAX_DELAY ); 
-        printf("Received notification %d times\n", count);
-    }
+    return a * b;
 }
 
+float sub ( float a , float b)
+{
+    return a - b;
+}
+
+float division ( float a , float b)
+{
+    return a / b;
+}
+
+calculator selectFunction(char operationSelector)
+{
+    calculator operationFunction;
+    switch (operationSelector)
+    {
+    case '+':
+        operationFunction = sum;
+        break;
+    case '-':
+        operationFunction = sub;
+        break;
+    case '*':
+        operationFunction = mult;
+        break;
+    case '/':
+        operationFunction = division;
+        break;
+    default:
+        operationFunction = NULL;
+        break;
+    }
+    return operationFunction;
+}
 void app_main(void)
 {
-    xTaskCreate(&receiver, "receiver", 2048, NULL, 2, &xReceiverHandler);
-    xTaskCreate(&sender, "sender", 2048, NULL, 2, &xSenderHandler);
+    /* Initializing buffer to write in the console*/
+    setvbuf(stdin, NULL, _IONBF, 0);
+    setvbuf(stdout, NULL, _IONBF, 0);
+    ESP_ERROR_CHECK(uart_driver_install(CONFIG_ESP_CONSOLE_UART_NUM, 256, 0, 0, NULL, 0));
+    esp_vfs_dev_uart_use_driver(CONFIG_ESP_CONSOLE_UART_NUM);
+    esp_vfs_dev_uart_port_set_rx_line_endings(CONFIG_ESP_CONSOLE_UART_NUM, ESP_LINE_ENDINGS_CR);
+    esp_vfs_dev_uart_port_set_tx_line_endings(CONFIG_ESP_CONSOLE_UART_NUM, ESP_LINE_ENDINGS_CRLF);
+    
+    calculator calc;
+    char selector;
+    bool proceed = true;
+    do
+    {
+        //menu();
+        printf("************CALCULATOR***********\n");
+        printf("Select the operation (+ , - , / , *):\n");
+        scanf("%c", &selector);
+        fflush(stdin);
+        calc = selectFunction(selector);
+        if (calc != NULL)
+        {
+            float a,b;
+            printf("Insert the first value: ");
+            scanf("%f", &a);
+            fflush(stdin);
+            printf("\nInsert the second value: ");
+            scanf("%f", &b);
+            fflush(stdin);
+            printf("\nThe result is %.2f\n", calc(a, b));
+        }
+        else 
+        {
+            printf("Invalid operation\n");
+        }
+
+        char cont;
+        printf("Do you want to continue? (Y for yes, N for no)\n");
+        scanf("%c", &cont);
+        fflush(stdin);
+        switch (cont)
+        {
+            case 'Y':
+            case 'y':
+                proceed = true;
+            break;
+            case 'N':
+            case 'n':
+                proceed = false;
+            break;
+            default:
+                printf("Invalid option, try again\n");
+            break;
+        }
+        
+    } while (proceed);
+    printf("\nCalculator finished\n");
 }
